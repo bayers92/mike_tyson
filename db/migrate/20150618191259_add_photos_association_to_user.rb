f7763752aa24f16a280e4a59688c8a7b5af2ea65
bayers92 <brianc.ayers@gmail.com>
2015-06-19 10:34:15 -0400

    removed migration files for photos and photo_album models

--- db/migrate/20150618191259_add_photos_association_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddPhotosAssociationToUser < ActiveRecord::Migration
-  def change
-  	  	add_column :photos, :user_id, :integer
-  	add_index :photos, :user_id
-  end
-end
--- db/migrate/20150619040851_drop_photo_album.rb
+++
@@ -1,5 +0,0 @@
-class DropPhotoAlbum < ActiveRecord::Migration
-  def change
-  	drop_table :photo_album
-  end
-end

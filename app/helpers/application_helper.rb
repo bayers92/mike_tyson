14d9cbefec0fd27b41a15f8d13b7259ec82bbd1f
bayers92 <brianc.ayers@gmail.com>
2015-06-22 11:27:29 -0400

    valid user checks and user edit landing screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
 	def active_class(link_path)
 	  current_page?(link_path) ? "active" : ""
 	 end
+
+
+	def valid_user(user)
+		if (user.homepage_info != nil && user.personal_info != nil && user.background_info != nil && user.showcase != nil && user.articles != nil && user.photo_link != nil && user.social_link != nil)
+			return true
+		end
+	end
 end

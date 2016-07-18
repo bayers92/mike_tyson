7969e9847c4bdd48b0a9af03eb56412f99240ce0
bayers92 <brianc.ayers@gmail.com>
2016-07-18 19:41:13 -0400

    larger photo dimensions

--- app/models/personal_info.rb
+++ app/models/personal_info.rb
@@ -4,20 +4,20 @@ class PersonalInfo < ActiveRecord::Base
 	has_attached_file :resume
 	validates_attachment_content_type :resume, :content_type => ["application/pdf"]
 
-	has_attached_file :about_pic, styles: { small: "64x64", med: "200x200", large: "300x300" }, default_url: "/images/:style/missing.png"
+	has_attached_file :about_pic, styles: { small: "64x64", med: "300x300", large: "500x500" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :about_pic, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	crop_attached_file :about_pic
 
-	has_attached_file :exp1, styles: { small: "64x64", med: "200x200", large: "300x300" }
+	has_attached_file :exp1, styles: { small: "64x64", med: "300x300", large: "500x500" }
 	validates_attachment_content_type :exp1, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-	has_attached_file :exp2, styles: { small: "64x64", med: "200x200", large: "300x300" }
+	has_attached_file :exp2, styles: { small: "64x64", med: "300x300", large: "500x500" }
 	validates_attachment_content_type :exp2, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-	has_attached_file :exp3, styles: { small: "64x64", med: "200x200", large: "300x300" }
+	has_attached_file :exp3, styles: { small: "64x64", med: "300x300", large: "500x500" }
 	validates_attachment_content_type :exp3, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-	has_attached_file :exp4, styles: {  small: "64x64", med: "200x200", large: "300x300" }
+	has_attached_file :exp4, styles: { small: "64x64", med: "300x300", large: "500x500" }
 	validates_attachment_content_type :exp4, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
